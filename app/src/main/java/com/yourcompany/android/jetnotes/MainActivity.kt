870246d732f9d2e1a791e09e529dea45acb93f97
Abdul <abdul.tanveer@gmail.com>
2023-12-20 12:03:27 +0800

    Adding Appdrawer to MainActivity

--- app/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
@@ -38,8 +38,20 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.DrawerState
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.yourcompany.android.jetnotes.routing.Screen
+import com.yourcompany.android.jetnotes.theme.JetNotesTheme
+import com.yourcompany.android.jetnotes.ui.components.AppDrawer
+import com.yourcompany.android.jetnotes.ui.components.Note
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
 
 /**
  * Main activity for the app.
@@ -61,6 +73,20 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
 
     setContent {
+      JetNotesTheme {
+        val  coroutineScope = rememberCoroutineScope()
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
+       // val drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed )
+
+        Scaffold(scaffoldState = scaffoldState,
+          drawerContent = {
+                          AppDrawer(currentScreen = Screen.Notes, onScreenSelected = {
+                            screen ->  coroutineScope.launch { scaffoldState.drawerState.close() }
+                          } )
+          },
+          content = { Note()})
+
+      }
 
     }
   }
