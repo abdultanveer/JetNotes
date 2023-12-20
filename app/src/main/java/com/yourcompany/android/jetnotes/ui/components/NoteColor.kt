9b49512a3cedd4c748893cce533b80690e408435
Abdul <abdul.tanveer@gmail.com>
2023-12-20 13:40:56 +0800

    NoteColor -- removed hardcoded values

--- app/src/main/java/com/yourcompany/android/jetnotes/ui/components/NoteColor.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/components/NoteColor.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -12,17 +13,19 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor() {
+fun NoteColor(color: Color, size: Dp, padding: Dp = 0.dp, border:Dp) {
     Box(modifier = Modifier
-        .size(40.dp)
+        .padding(padding)
+        .size(size)
         .clip(CircleShape)
         .background(Color.Red)
         .border(
             BorderStroke(
-                width = 2.dp,
+                border,
                 SolidColor(Color.Black)), CircleShape
         )
     )
@@ -32,5 +35,5 @@ fun NoteColor() {
 @Preview
 @Composable
 fun NoteColorPreview() {
-    NoteColor()
+    NoteColor( color =  Color.Red, size =  40.dp, padding = 4.dp, border = 2.dp)
 }
