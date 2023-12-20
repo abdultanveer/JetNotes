faa29f8ded968ec6f1f05dad6bc9eade56dc8f40
Abdul <abdul.tanveer@gmail.com>
2023-12-20 16:18:59 +0800

    MainViewModel -- emit list<NoteModel> and receive 3 events

--- app/src/main/java/com/yourcompany/android/jetnotes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/viewmodel/MainViewModel.kt
@@ -36,8 +36,11 @@ package com.yourcompany.android.jetnotes.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.yourcompany.android.jetnotes.data.repository.Repository
 import com.yourcompany.android.jetnotes.domain.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * View model used for storing the global app state.
@@ -47,6 +50,14 @@ import com.yourcompany.android.jetnotes.domain.model.NoteModel
 class MainViewModel(private val repository: Repository) : ViewModel() {
     val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
         repository.getAllNotesNotInTrash().asLiveData()
+    }
+
+    fun onCreateNewNoteClick(){}
+    fun onNoteClick(){}
+    fun onNoteCheckChanged(note: NoteModel){
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.insertNote(note)
+        }
     }
 
 }
