8bd97912f04115e963db99d43851756a6e7910c4
Abdul <abdul.tanveer@gmail.com>
2023-12-20 15:33:26 +0800

    ColorPicker

--- app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
@@ -1,14 +1,17 @@
 package com.yourcompany.android.jetnotes.ui.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,8 +20,29 @@ import com.yourcompany.android.jetnotes.ui.components.NoteColor
 import com.yourcompany.android.jetnotes.util.fromHex
 
 @Composable
-fun ColorPicker() {
+fun ColorPicker(colors:List<ColorModel>, onColorSelect: (ColorModel) -> Unit) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "color picker", fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(modifier = Modifier.fillMaxWidth()){
+            items(colors.size){itemIndex->
+                val color = colors[itemIndex]
+                ColorItem(color = color, onColorSelect = onColorSelect)
+            }
+        }
+    }
+
     
+}
+
+@Preview
+@Composable
+fun ColorPickerPreview(){
+    ColorPicker(colors = listOf(
+            ColorModel.DEFAULT,ColorModel.DEFAULT,ColorModel.DEFAULT
+        ), onColorSelect = {} )
 }
 
 
